9a0aa69d63cf819601f1cd7a67263f0bb5e17a1e
Bifumen <38299444+Bifumen@users.noreply.github.com>
2018-06-17 17:44:04 +0200

    Fixed bugs
    Fixed a bug where the Audio Source Element prefab didn't detect the sript. Also added volume sliders for audio library

--- Audio Manager/Scripts/Sound.cs
+++ Audio Manager/Scripts/Sound.cs
@@ -9,11 +9,18 @@ namespace AudioPoolingSystem
     public class Sound
     {
         public string name;
+
         [Space]
         public AudioClip clip;
+
+        [Range(0, 1)]
         public float volume;
+
+        [Range(0, 2)]
         public float pitch;
+
         public bool loop;
+
         [Space]
         public AudioMixerGroup mixer;
     }
