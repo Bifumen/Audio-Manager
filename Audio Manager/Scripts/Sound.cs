10e50c2eedbd01ec30ff857e52a7f3870e42dd93
Bifumen <38299444+Bifumen@users.noreply.github.com>
2018-07-01 23:46:36 +0200

    Add files via upload

---
+++ Audio Manager/Scripts/Sound.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace AudioPoolingSystem
+{
+    [System.Serializable]
+    public class Sound
+    {
+        public string name;
+
+        [Space]
+        public AudioClip clip;
+
+        [Range(0, 1)]
+        public float volume;
+
+        [Range(0, 2)]
+        public float pitch;
+
+        public bool loop;
+
+        [Space]
+        public AudioMixerGroup mixer;
+
+        public Sound(AudioClip audioClip,bool willLoop)
+        {
+            clip = audioClip;
+            volume = 1;
+            pitch = 1;
+            loop = willLoop;
+        }
+
+        public Sound(AudioClip audioClip, bool willLoop, float audioVolume,float audioPitch,AudioMixerGroup mixerGroup)
+        {
+            clip = audioClip;
+            volume = audioVolume;
+            pitch = audioPitch;
+            loop = willLoop;
+            mixer = mixerGroup;
+        }
+    }
+}
